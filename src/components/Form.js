ad0e1c11904061de1e6b175e7e69ca4c88e8a3df
Jweb60 <jy.babiarczyk@gmail.com>
2022-07-03 10:11:48 +0200

    debut card

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,6 +1,18 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import Card from "./Card";
 
 const Form = () => {
+  const [moviesData, setMoviesData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.themoviedb.org/3/search/movie?api_key=2198f9ca16c7c38b61929e13298b9f6c&query=star&language=fr-FR"
+      )
+      .then((res) => setMoviesData(res.data.results));
+  }, []);
+
   return (
     <div className="form-component">
       <div className="form-container">
@@ -15,6 +27,11 @@ const Form = () => {
           <btn-sort id="badToGod">Flop</btn-sort>
           <span>➟</span>
         </div>
+      </div>
+      <div className="result">
+        {moviesData.slice(0, 12).map((movie) => (
+          <Card key={movie.id} movie={movie}/>
+        ))}
       </div>
     </div>
   );
